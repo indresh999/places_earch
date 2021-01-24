d1df16142af9637c91a38f38793bdfe45900bf4a
indresh999 <indresh.malviya@gmail.com>
2021-01-24 20:34:11 +0530

    new code

--- app/src/main/java/com/teetask/placesdk/MainActivity.java
+++ app/src/main/java/com/teetask/placesdk/MainActivity.java
@@ -1,82 +1,86 @@
 package com.teetask.placesdk;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import android.graphics.Bitmap;
-import android.os.Bundle;
+import android.content.Intent;
 
-import android.widget.ImageView;
-import android.widget.Toast;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.common.api.Status;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
-import com.google.android.libraries.places.api.model.TypeFilter;
-import com.google.android.libraries.places.api.net.FetchPhotoRequest;
-import com.google.android.libraries.places.api.net.FetchPhotoResponse;
 import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
-import android.os.Bundle;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+
 import java.util.Arrays;
-import java.util.Objects;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    PlacesClient placesClient;
+    private static final String GOOGLE_PLACES_API_KEY = "AIzaSyBRr5WRFRbaVwLQ3Sqe4pmfQhCQwAxEj4E";
+    private static final int AUTOCOMPLETE_REQUEST_CODE = 123;
+    private TextView tvLocationName, tvPlaceId, tvLatLon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Places.initialize(getApplicationContext(), GOOGLE_PLACES_API_KEY);
 
+        tvLocationName = findViewById(R.id.textView6);
+        tvPlaceId = findViewById(R.id.textView7);
+        tvLatLon = findViewById(R.id.textView5);
 
-        String apiKey = getString(R.string.api_key);
-        if (!Places.isInitialized()) {
-            Places.initialize(getApplicationContext(), apiKey);
-        }
-
-        // Create a new Places client instance.
-        placesClient = Places.createClient(this);
-
-
-        // Initialize the AutocompleteSupportFragment.
-        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
-                getSupportFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
-        autocompleteFragment.setTypeFilter(TypeFilter.CITIES);
-        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.PHOTO_METADATAS));
-        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+        Button btnPlaces = findViewById(R.id.btnPlaces);
+        btnPlaces.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onPlaceSelected(@NonNull Place place) {
-                // TODO: Get info about the selected place.
-                Toast.makeText(getApplicationContext(), place.getName(), Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                createAutoCompleteIntent();
 
-                FetchPhotoRequest photoRequest = FetchPhotoRequest.builder(Objects.requireNonNull(place.getPhotoMetadatas()).get(0))
-                        .build();
-                placesClient.fetchPhoto(photoRequest).addOnSuccessListener(
-                        new OnSuccessListener<FetchPhotoResponse>() {
-                            @Override
-                            public void onSuccess(FetchPhotoResponse response) {
-                                Bitmap bitmap = response.getBitmap();
-                                ((ImageView)findViewById(R.id.img)).setImageBitmap(bitmap);
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception exception) {
-                                exception.printStackTrace();
-                            }
-                        });
-            }
-
-            @Override
-            public void onError(@NonNull Status status) {
-                // TODO: Handle the error.
-                Toast.makeText(getApplicationContext(), status.toString(), Toast.LENGTH_SHORT).show();
             }
         });
 
-
     }
+
+    private void createAutoCompleteIntent() {
+        tvLocationName.setText("");
+        tvPlaceId.setText("");
+        tvLatLon.setText("");
+        List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG);
+        Intent intent = new Autocomplete.IntentBuilder(
+                AutocompleteActivityMode.FULLSCREEN, fields)
+                .build(MainActivity.this);
+        startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Place place = Autocomplete.getPlaceFromIntent(data);
+                Log.i("tag", "Place: " + place.getName() + ", " + place.getLatLng());
+                tvLocationName.setText(place.getName());
+                tvPlaceId.setText(place.getId());
+                tvLatLon.setText(String.valueOf(place.getLatLng()));
+
+            } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+                // TODO: Handle the error.
+                Status status = Autocomplete.getStatusFromIntent(data);
+                Log.i("tag", status.getStatusMessage());
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
+    }
+
+
 }
